e231461bb2292b529d09a1491bc60c08a6ec27c2
$@ID <73528996+00009713@users.noreply.github.com>
2021-12-09 23:44:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -20,3 +20,27 @@ slides.forEach(slide => {
     observer.observe(slide);
 });
 
+// Dark mode
+
+const bwButton = document.querySelector('#bwButton');
+const mapBase = document.querySelector('.map-base');
+const mapBorder = document.querySelector('.map-border');
+
+bwButton.addEventListener("click", () => {
+    bwButton.classList.toggle('active');
+    if (bwButton.classList.contains('active') === true) {
+        document.documentElement.style.setProperty('--main-color', 'black');
+        document.documentElement.style.setProperty('--color-invert', 'invert(0%)');
+        document.documentElement.style.setProperty('--backgroud-color', 'white');
+        document.documentElement.style.setProperty('--font-weight', 'normal');
+        mapBase.style.display = 'block';
+        mapBorder.style.display = 'none';
+    } else {
+        document.documentElement.style.setProperty('--main-color', 'white');
+        document.documentElement.style.setProperty('--color-invert', 'invert(100%)');
+        document.documentElement.style.setProperty('--backgroud-color', 'black');
+        document.documentElement.style.setProperty('--font-weight', 'lighter');
+        mapBase.style.display = 'none';
+        mapBorder.style.display = 'block';
+    }
+});
